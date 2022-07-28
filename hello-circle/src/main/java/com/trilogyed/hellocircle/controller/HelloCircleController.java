8d910cf46b2d79739b081258d4b24737d094640b
paperpatch <patchen21@gmail.com>
2022-07-28 16:47:04 -0400

    add code to pass test on /answer endpoint

--- hello-circle/src/main/java/com/trilogyed/hellocircle/controller/HelloCircleController.java
+++ hello-circle/src/main/java/com/trilogyed/hellocircle/controller/HelloCircleController.java
@@ -10,4 +10,7 @@ public class HelloCircleController {
     public String helloCircle(){
         return "Hello, Circle!";
     }
+
+    @GetMapping(value = "/answer")
+    public String getAnswer() { return "The answer is 42!"; }
 }
